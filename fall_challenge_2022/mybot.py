f40ef5c5765a10862528c0ec16ed1c38557efced
Matt Matt <matthieu.chambery@gmail.com>
2023-01-04 22:37:31 +0100

    debug on printing the computation time

--- fall_challenge_2022/mybot.py
+++ fall_challenge_2022/mybot.py
@@ -494,6 +494,7 @@ while True:
     if actions == "":
         actions = "WAIT"
     end_time = time.time_ns()
-    print(actions + f"MESSAGE {str((end_time-start_time)/1000000)}")
+    print(f"{str((end_time - start_time) / 1000000)}", file=sys.stderr, flush=True)
+    print(actions)
 
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
