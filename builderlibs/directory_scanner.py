7efa3b57d9cb920f263eb70680936d21a2b89023
Matthieu Chambery <mchambery@betomorrow.com>
2023-01-09 16:33:48 +0100

    change path handling not to be dependant on OS

--- builderlibs/directory_scanner.py
+++ builderlibs/directory_scanner.py
@@ -2,14 +2,14 @@ import os
 from pathlib import Path
 from typing import List, Dict
 
-ABSPATH = Path(__file__).parent.resolve()
-DIRECTORY_PATH = os.path.join(ABSPATH, "..\\bots")
+ABSPATH = Path(__file__).parent.parent.resolve()
+DIRECTORY_PATH = os.path.join(ABSPATH, "bots")
 
 
 class DirectoryScanner:
     def __init__(self):
         self._challenges_names: List[str] = os.listdir(DIRECTORY_PATH)
-        self.challenges_paths: Dict[str, str] = {challenge_name: os.path.join(DIRECTORY_PATH, challenge_name) \
+        self.challenges_paths: Dict[str, str] = {challenge_name: Path(os.path.join(DIRECTORY_PATH, challenge_name)).resolve() \
                                                   for challenge_name in self._challenges_names}
 
     @property
@@ -24,7 +24,7 @@ class DirectoryScanner:
 
     def get_bot_file_path(self, challenge_name: str, bot_file_name: str) -> str:
         challenge_path = self.get_challenge_path(challenge_name)
-        bot_file_path = os.path.join(challenge_path, bot_file_name)
+        bot_file_path = Path(os.path.join(challenge_path, bot_file_name)).resolve()
         if not os.path.isfile(bot_file_path):
             raise FileNotFoundError(f"File {bot_file_name} not found in challenge directory {challenge_path}.")
         return bot_file_path
