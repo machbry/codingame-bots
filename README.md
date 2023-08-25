5767d9e2ba30540ea1eb26500b403b796aae1e5a
CHAMBERY Matthieu <matthieu.chambery@gmail.com>
2023-08-25 17:34:49 +0200

    build script to make or destroy a challenge folder

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### This project is to help & fasten bot development for codingame (python) :
-1. run build.py -c <challenge_name> to build a one-file ready to be submitted
+1. run src/build.py -c <challenge_name>
 2. use only explicit imports for custom libs in bot.py (from ... import ...)
---
+++ src/build.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+from builderlibs.challenge import ChallengeFolder
+
+
+UNBUILT_BOTS_DIRECTORY = Path(__file__).parent.parent.resolve() / "bots"
+
+
+parser = ArgumentParser()
+parser.add_argument("-c", "--challenge_name", type=str)
+parser.add_argument("-m", "--make", type=bool, default=True)
+parser.add_argument("-d", "--destroy", type=bool, default=False)
+arguments = parser.parse_args().__dict__
+
+challenge_name = arguments["challenge_name"]
+make_challenge = arguments["make"]
+destroy_challenge = arguments["destroy"]
+
+challenge_folder = ChallengeFolder(name=challenge_name, parent=UNBUILT_BOTS_DIRECTORY)
+if make_challenge:
+    challenge_folder.make()
+if destroy_challenge:
+    if challenge_folder.exists():
+        challenge_folder.destroy(force_destroy=False)
+
+# pseudo code to build a unique bot file
