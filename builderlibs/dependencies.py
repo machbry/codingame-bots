8f4df6bc49c81fec413f6b2ccc6263be727ea5e1
CHAMBERY Matthieu <matthieu.chambery@gmail.com>
2023-09-01 20:24:30 +0200

    add ImportStatement, Import & ImportFrom

--- builderlibs/dependencies.py
+++ builderlibs/dependencies.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List, Union
+import ast
 
 
 @dataclass
@@ -15,3 +17,28 @@ class Module:
     def is_local(self, imported_from: Path, level: int) -> bool:
         path_to_check = self._path_to_check(imported_from=imported_from, level=level)
         return path_to_check.is_file()
+
+
+class ImportStatement:
+    def __init__(self, ast_node: Union[ast.Import, ast.ImportFrom], file_path: Path):
+        self._ast_node = ast_node
+        self._file_path = file_path
+        self._modules: List[Module] = []
+        self._level = 0
+
+    @property
+    def modules(self) -> List[Module]:
+        return self._modules
+
+
+class Import(ImportStatement):
+    def __init__(self, ast_node: ast.Import, file_path: Path):
+        super().__init__(ast_node=ast_node, file_path=file_path)
+        self._modules = [Module(name=alias.name, asname=alias.asname) for alias in self._ast_node.names]
+
+
+class ImportFrom(ImportStatement):
+    def __init__(self, ast_node: ast.ImportFrom, file_path: Path):
+        super().__init__(ast_node=ast_node, file_path=file_path)
+        self._modules = [Module(name=self._ast_node.module)]
+        self._level = ast_node.level
