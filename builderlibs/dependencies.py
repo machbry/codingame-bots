29fbf19f455e70f158c5eaa13c982ae86119d4c2
Matt Matt <matthieu.chambery@gmail.com>
2023-09-26 00:02:19 +0200

    add docs + fix to visit_importFrom

--- builderlibs/dependencies.py
+++ builderlibs/dependencies.py
@@ -73,6 +73,15 @@ class ImportFrom(ImportStatement):
 
 
 class LocalModuleImportReplacer(ast.NodeTransformer):
+    """
+    A class used to replace local module imports with the corresponding module from the local package.
+
+    Attributes:
+        _main_module (LocalModule): The main module to replace local imports in.
+        _local_packages_paths (List[Path]): A list of paths to local packages.
+        _local_modules_replaced (List[LocalModule]): A list of local modules that have already been replaced.
+    """
+
     def __init__(self, main_module: LocalModule, local_packages_paths: List[Path] = []):
         super().__init__()
         self._main_module = main_module
@@ -80,6 +89,17 @@ class LocalModuleImportReplacer(ast.NodeTransformer):
         self._local_modules_replaced = []
 
     def visit_ImportFrom(self, node: ImportFrom) -> Union[ast.ImportFrom, ast.Module]:
+        """
+        Visits an ImportFrom node in the AST and replaces any local module imports with the corresponding
+        module from the local package.
+
+        Args:
+            node (ast.ImportFrom): The ImportFrom node to visit.
+
+        Returns:
+            Union[ast.ImportFrom, ast.Module]: The modified ImportFrom node or an empty Module node if the
+            imported module is not local.
+        """
         from_paths = [self._main_module.file_path] + self._local_packages_paths
 
         for from_path in from_paths:
@@ -92,15 +112,27 @@ class LocalModuleImportReplacer(ast.NodeTransformer):
                     target_module_file = PythonFile(target_path)
                     local_module_to_import = LocalModule(target_module_file)
 
-                    replacer = LocalModuleImportReplacer(main_module=local_module_to_import)
+                    replacer = LocalModuleImportReplacer(main_module=local_module_to_import, local_packages_paths=self._local_packages_paths)
 
                     return ast.fix_missing_locations(replacer.visit(local_module_to_import.tree))
                 else:
                     return ast.Module(body=[], type_ignores=[])
-            
+
         return node
 
     def visit_Import(self, node: Import):
+        """
+        Visits an Import node in the AST and raises an error if the imported module is local.
+
+        Args:
+            node (ast.Import): The Import node to visit.
+
+        Raises:
+            ValueError: If the imported module is local.
+
+        Returns:
+            ast.Import: The original Import node.
+        """
         main_module_path = self._main_module.file_path
         imported_modules = Import(node=node, from_path=main_module_path).modules
 
@@ -113,12 +145,33 @@ class LocalModuleImportReplacer(ast.NodeTransformer):
 
 
 class ModuleAggregater:
+    """
+    A class that aggregates a main module and its local dependencies into a single AST.
+
+    :param main_module: The main module to aggregate.
+    :type main_module: LocalModule
+    :param local_packages_paths: A list of paths to local packages that the main module depends on.
+    :type local_packages_paths: List[Path]
+    """
+
     def __init__(self, main_module: LocalModule, local_packages_paths: List[Path] = []):
         self._main_module = main_module
         self._replacer = LocalModuleImportReplacer(main_module, local_packages_paths)
 
     def aggregate(self) -> ast.AST:
+        """
+        Aggregates the main module's abstract syntax tree by visiting it with the replacer and fixing any missing locations.
+
+        Returns:
+            The aggregated abstract syntax tree.
+        """
         return ast.fix_missing_locations(self._replacer.visit(self._main_module.tree))
 
     def to_source(self) -> str:
+        """
+        Returns the source code representation of the AST node.
+
+        :return: A string containing the source code representation of the AST node.
+        :rtype: str
+        """
         return ast.unparse(self.aggregate())
