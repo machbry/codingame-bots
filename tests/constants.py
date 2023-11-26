5a7c7b7010033d83f448b202c4640cbd94b9239a
Matt Matt <matthieu.chambery@gmail.com>
2023-11-26 11:17:08 +0100

    add test for full challenge build

--- tests/constants.py
+++ tests/constants.py
@@ -4,6 +4,7 @@ from pathlib import Path
 TESTS_ROOT_PATH = Path(__file__).parent.resolve()
 TESTS_DATA_PATH = TESTS_ROOT_PATH / "data"
 TESTS_RES_PATH = TESTS_ROOT_PATH / "res"
-TESTS_BOTS_PATH = TESTS_ROOT_PATH / "res" / "bots"
+TESTS_BOTS_PATH = TESTS_RES_PATH / "bots"
+TESTS_SHAREDLIBS_PATH = TESTS_RES_PATH / "sharedlibs"
 
 TESTS_DATA_PATH.mkdir(exist_ok=True, parents=True)
--- tests/test_builderlibs/test_aggregater.py
+++ tests/test_builderlibs/test_aggregater.py
@@ -1,7 +1,7 @@
 import pytest
 
 from builderlibs.aggregater import LocalModuleImportReplacer
-from constants import TESTS_RES_PATH
+from constants import TESTS_RES_PATH, TESTS_SHAREDLIBS_PATH, TESTS_BOTS_PATH
 
 
 @pytest.fixture
@@ -16,8 +16,15 @@ def local_module_import_replacer(test_challenge):
     ("import sharedlibs", ValueError),
     ("import pandas as pd", None)
 ])
-def test_replacer_visit_import(import_statement, exception_expected, local_module_import_replacer, create_ast_import_node):
+def test_replacer_visit_import(import_statement, exception_expected, local_module_import_replacer,
+                               create_ast_import_node):
     import_node = create_ast_import_node(import_statement)
     if exception_expected:
         with pytest.raises(ValueError):
             local_module_import_replacer.visit_Import(import_node)
+
+
+def test_challenge_build(test_challenge):
+    challenge_source = test_challenge.aggregate_to_source(local_packages_paths=[TESTS_SHAREDLIBS_PATH])
+    with open(TESTS_BOTS_PATH / (test_challenge.name + ".py"), "w") as f:
+        f.write(challenge_source)
