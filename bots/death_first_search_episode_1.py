f6d1d8999de0b56596fbac0db34b4dc5eb98c287
Matt Matt <matthieu.chambery@gmail.com>
2023-12-15 19:57:22 +0100

    init death_first_search_episode_1 challenge

---
+++ bots/death_first_search_episode_1.py
@@ -0,0 +1,10 @@
+import math
+import sys
+(n, l, e) = [int(i) for i in input().split()]
+for i in range(l):
+    (n1, n2) = [int(j) for j in input().split()]
+for i in range(e):
+    ei = int(input())
+while True:
+    si = int(input())
+    print('0 1')
