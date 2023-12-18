d52ef301ee626d21bd139fe225808cf9b6b5817a
Matt Matt <matthieu.chambery@gmail.com>
2023-12-18 19:11:38 +0100

    get and log game inputs

--- bots/fall_challenge_2023_built.py
+++ bots/fall_challenge_2023_built.py
@@ -4,19 +4,62 @@ from typing import List
 class GameLoop:
     RUNNING = True
     LOG = True
-    RESET_TURNS_INPUTS = False
+    RESET_TURNS_INPUTS = True
 
     def __init__(self):
         self.init_inputs: List[str] = []
+        self.nb_turns: int = 0
         self.turns_inputs: List[str] = []
+        creature_count = int(self.get_init_input())
+        for i in range(creature_count):
+            (creature_id, color, _type) = [int(j) for j in self.get_init_input().split()]
         if GameLoop.LOG:
             print(self.init_inputs, file=sys.stderr, flush=True)
 
+    def get_init_input(self):
+        result = input()
+        self.init_inputs.append(result)
+        return result
+
+    def get_turn_input(self):
+        result = input()
+        self.turns_inputs.append(result)
+        return result
+
     def start(self):
         while GameLoop.RUNNING:
-            self.turns_inputs.append(input())
+            self.nb_turns += 1
+            my_score = int(self.get_turn_input())
+            foe_score = int(self.get_turn_input())
+            my_scan_count = int(self.get_turn_input())
+            for i in range(my_scan_count):
+                creature_id = int(self.get_turn_input())
+            foe_scan_count = int(self.get_turn_input())
+            for i in range(foe_scan_count):
+                creature_id = int(self.get_turn_input())
+            my_drone_count = int(self.get_turn_input())
+            for i in range(my_drone_count):
+                (drone_id, drone_x, drone_y, emergency, battery) = [int(j) for j in self.get_turn_input().split()]
+            foe_drone_count = int(self.get_turn_input())
+            for i in range(foe_drone_count):
+                (drone_id, drone_x, drone_y, emergency, battery) = [int(j) for j in self.get_turn_input().split()]
+            drone_scan_count = int(self.get_turn_input())
+            for i in range(drone_scan_count):
+                (drone_id, creature_id) = [int(j) for j in self.get_turn_input().split()]
+            visible_creature_count = int(self.get_turn_input())
+            for i in range(visible_creature_count):
+                (creature_id, creature_x, creature_y, creature_vx, creature_vy) = [int(j) for j in self.get_turn_input().split()]
+            radar_blip_count = int(self.get_turn_input())
+            for i in range(radar_blip_count):
+                inputs = self.get_turn_input().split()
+                drone_id = int(inputs[0])
+                creature_id = int(inputs[1])
+                radar = inputs[2]
             if GameLoop.LOG:
+                print(self.nb_turns, file=sys.stderr, flush=True)
                 print(self.turns_inputs, file=sys.stderr, flush=True)
             if GameLoop.RESET_TURNS_INPUTS:
                 self.turns_inputs = []
+            for i in range(my_drone_count):
+                print('WAIT 1')
 GameLoop().start()
--- tests/test_bots/fall_challenge_2023/test_bot.py
+++ tests/test_bots/fall_challenge_2023/test_bot.py
@@ -6,10 +6,12 @@ from bots.fall_challenge_2023.challengelibs.game_loop import GameLoop
 
 
 CHALLENGELIBS_PACKAGE = "bots.fall_challenge_2023.challengelibs"
+GameLoop.LOG = False
 
 
 @pytest.mark.parametrize("init_inputs, nb_turns, turns_inputs", [
-    (['0'], 1, ['1'])
+    (['12', '2 0 0', '3 1 0', '4 0 1', '5 1 1', '6 0 2', '7 1 2', '8 2 0', '9 3 0', '10 2 1', '11 3 1', '12 2 2', '13 3 2'],
+     1, ['0', '0', '0', '0', '1', '0 3333 500 0 30', '1', '1 6666 500 0 30', '0', '12', '2 1625 3881 -200 0', '3 8374 3881 200 0', '4 1409 6412 141 141', '5 8590 6412 -141 141', '6 1313 8792 200 0', '7 8686 8792 -200 0', '8 6778 3650 -141 -141', '9 3221 3650 141 -141', '10 2489 6061 -141 141', '11 7510 6061 141 141', '12 2713 8743 0 -200', '13 7286 8743 0 -200', '12', '0 2 BL', '0 3 BR', '0 4 BL', '0 5 BR', '0 6 BL', '0 7 BR', '0 8 BR', '0 9 BL', '0 10 BL', '0 11 BR', '0 12 BL', '0 13 BR'])
 ])
 def test_replay_turns(init_inputs: List[str], nb_turns: int, turns_inputs: List[str]):
     input_side_effect = [*init_inputs, *turns_inputs]
