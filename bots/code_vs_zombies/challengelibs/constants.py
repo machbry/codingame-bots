1229ffa9b6eceea7ac55ef5d6424f9e4e3087a22
Matt Matt <matthieu.chambery@gmail.com>
2023-12-23 15:25:43 +0100

    fix use of HashMapNorms

--- bots/code_vs_zombies/challengelibs/constants.py
+++ bots/code_vs_zombies/challengelibs/constants.py
@@ -1,7 +1,7 @@
 from botlibs.trigonometry import HashMapNorms, Vector
 
 
-HASH_MAP_NORMS = HashMapNorms()
+HASH_MAP_NORMS = HashMapNorms(norm_name="norm2")
 X_MAX = 16000
 Y_MAX = 9000
 D_MAX = HASH_MAP_NORMS[Vector(X_MAX, Y_MAX)]
