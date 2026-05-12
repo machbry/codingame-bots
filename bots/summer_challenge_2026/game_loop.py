12e88a968cccc3640e2f1db836cc00dd3645f2b2
machbry <matthieu.chambery@gmail.com>
2026-05-12 23:37:46 +0200

    summer 2026 : handle real inputs

--- bots/summer_challenge_2026/game_loop.py
+++ bots/summer_challenge_2026/game_loop.py
@@ -4,7 +4,7 @@ from bots.summer_challenge_2026.challengelibs.logger import log
 
 
 class GameLoop:
-    __slots__ = ("init_inputs", "nb_turns", "turns_inputs", "actions")
+    __slots__ = ('init_inputs', 'nb_turns', 'turns_inputs', 'actions', 'width', 'height', 'lines')
 
     RUNNING = True
     LOG = True
@@ -15,6 +15,12 @@ class GameLoop:
         self.nb_turns: int = 0
         self.turns_inputs: List[str] = []
         self.actions: list[str] = []
+
+        self.width, self.height = [int(i) for i in self.get_init_input().split()]
+        self.lines = []
+        for i in range(self.height):
+            line = self.get_init_input()
+            self.lines.append(line)
 
         if GameLoop.LOG:
             self.print_init_logs()
@@ -41,10 +47,30 @@ class GameLoop:
     def update_assets(self):
         self.nb_turns += 1
 
+        for i in range(2):
+            plum, lemon, apple, banana, iron, wood = [int(j) for j in self.get_turn_input().split()]
+        
+        trees_count = int(self.get_turn_input())
+        for i in range(trees_count):
+            inputs = self.get_turn_input().split()
+            _type = inputs[0]
+            x = int(inputs[1])
+            y = int(inputs[2])
+            size = int(inputs[3])
+            health = int(inputs[4])
+            fruits = int(inputs[5])
+            cooldown = int(inputs[6])
+        
+        trolls_count = int(self.get_turn_input())
+        for i in range(trolls_count):
+            _id, player, x, y, movement_speed, carry_capacity, harvest_power, chop_power, carry_plum, carry_lemon, carry_apple, carry_banana, carry_iron, carry_wood = [int(j) for j in self.get_turn_input().split()]
+
         if GameLoop.LOG:
             self.print_turn_logs()
 
     def start(self):
         while GameLoop.RUNNING:
             self.update_assets()
+
+            print("MOVE 0 7 7")
 
