756e48592b6d6faf813ee0f194ea96ce38fed389
machbry <matthieu.chambery@gmail.com>
2026-05-12 23:21:55 +0200

    summer 2026 : init

---
+++ bots/summer_challenge_2026/game_loop.py
@@ -0,0 +1,50 @@
+from typing import List
+
+from bots.summer_challenge_2026.challengelibs.logger import log
+
+
+class GameLoop:
+    __slots__ = ("init_inputs", "nb_turns", "turns_inputs", "actions")
+
+    RUNNING = True
+    LOG = True
+    RESET_TURNS_INPUTS = True
+
+    def __init__(self):
+        self.init_inputs: List[str] = []
+        self.nb_turns: int = 0
+        self.turns_inputs: List[str] = []
+        self.actions: list[str] = []
+
+        if GameLoop.LOG:
+            self.print_init_logs()
+
+    def get_init_input(self):
+        result = input()
+        self.init_inputs.append(result)
+        return result
+
+    def get_turn_input(self):
+        result = input()
+        self.turns_inputs.append(result)
+        return result
+
+    def print_init_logs(self):
+        log(self.init_inputs)
+
+    def print_turn_logs(self):
+        log(self.nb_turns)
+        log(self.turns_inputs)
+        if GameLoop.RESET_TURNS_INPUTS:
+            self.turns_inputs = []
+
+    def update_assets(self):
+        self.nb_turns += 1
+
+        if GameLoop.LOG:
+            self.print_turn_logs()
+
+    def start(self):
+        while GameLoop.RUNNING:
+            self.update_assets()
+
